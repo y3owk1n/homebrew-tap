2e5d3a8dc2b0be13a5ab1acc892a590b5ca8757d
y3owk1n <wongyeowkin@gmail.com>
2024-12-01 18:48:12 +0800

    feat: bump version to 1.4.1

--- cpenv.rb
+++ cpenv.rb
@@ -3,18 +3,18 @@ class Cpenv < Formula
   homepage "https://github.com/y3owk1n/cpenv"
   
   # You'll replace these in your actual implementation
-  version "1.4.0"
+  version "1.4.1"
   
   # For macOS Intel (x86_64)
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/y3owk1n/cpenv/releases/download/v#{version}/cpenv-darwin-x64"
-    sha256 "0fe39f918fec119a105e0ea58e530299436cd89eb9639750384378490589e0ea"
+    sha256 "3edc67111bbb085e04437d034f520a8c6fb58f441b4e50825c680291e64783ec"
   end
   
   # For macOS Apple Silicon (arm64)
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/y3owk1n/cpenv/releases/download/v#{version}/cpenv-darwin-arm64"
-    sha256 "16ae06c740ae9f9e8d0fe9719a0e52cf91908967f336657cfec6a724305f9732"
+    sha256 "a179192fec7658824cf30acfe25be05de7f0a8a628342d5aec9ce94701384e3f"
   end
 
   def install
