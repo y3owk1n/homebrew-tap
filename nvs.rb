69a851b1377da25bb679eee7185cc06b3940892b
y3owk1n <wongyeowkin@gmail.com>
2025-03-08 19:46:23 +0800

    feat(nvs): update to 1.6.0

--- nvs.rb
+++ nvs.rb
@@ -1,18 +1,18 @@
 class Nvs < Formula
   desc "Neovim version switcher"
   homepage "https://github.com/y3owk1n/nvs"  # Replace with your actual URL
-  version "1.5.2"
+  version "1.6.0"
 
   # For macOS Intel (x86_64)
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/y3owk1n/nvs/releases/download/v#{version}/nvs-darwin-amd64"
-    sha256 "30cc658c3bcec579a8a1fb284c287b6376dbfcf2041def7e0cfd24a7bc1cf4ca"
+    sha256 "cebd84a8b32c1fec1b931b8e3e9afbe91181a8f2862706715821c9c0622186bd"
   end
 
   # For macOS Apple Silicon (arm64)
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/y3owk1n/nvs/releases/download/v#{version}/nvs-darwin-arm64"
-    sha256 "935dceee717daeda6bb4b0968c0b15b72a9a105c7f2ca0b87177fa5215a6dbab"
+    sha256 "8e85f4316af76744f9e317510e73a7df2645eccba34c2d0fa51be3ef05cfdc57"
   end
 
   def install
