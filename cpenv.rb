162ca96dbc1b021eaa7d9cd3788c5c0b432a07cd
mtraworld <kylewong@traworld.com>
2025-03-11 12:15:44 +0800

    feat(cpenv): update to 1.15.5

--- cpenv.rb
+++ cpenv.rb
@@ -3,18 +3,18 @@ class Cpenv < Formula
   homepage "https://github.com/y3owk1n/cpenv"
 
   # You'll replace these in your actual implementation
-  version "1.15.4"
+  version "1.15.5"
 
   # For macOS Intel (x86_64)
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/y3owk1n/cpenv/releases/download/v#{version}/cpenv-darwin-amd64"
-    sha256 "276a314ebc8382b36b6e4c7589248e10086c8d5aed2d7dd8420f96af58daa1fa"
+    sha256 "0de5e266316dec52208f4d4d4c9cad24e87e5afd3e549776066f6124c81336e3"
   end
 
   # For macOS Apple Silicon (arm64)
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/y3owk1n/cpenv/releases/download/v#{version}/cpenv-darwin-arm64"
-    sha256 "6f632e307bdfcc436f3c121aeed912e1d4db6f8a8b84f4b7d612a7c0da7e8527"
+    sha256 "5de64c6ab7c4dde5c6e4673163849a1aa55e5f5ffd68f463785f53a5c53e9e78"
   end
 
   def install
