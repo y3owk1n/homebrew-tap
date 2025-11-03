b11796fd20c333b042fb9da5e8676a92d2f282b2
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-11-04 00:39:21 +0800

    feat(govim): update to 1.3.0

--- govim.rb
+++ govim.rb
@@ -1,18 +1,18 @@
 class Govim < Formula
 	desc "Govim - keyboard driven navigation for MacOS"
 	homepage "https://github.com/y3owk1n/govim"
-	version "1.2.0"
+	version "1.3.0"
 
 	# For macOS Intel (x86_64)
 	if OS.mac? && Hardware::CPU.intel?
 		url "https://github.com/y3owk1n/govim/releases/download/v#{version}/govim-darwin-amd64"
-		sha256 "0d3e1d11697e0f39aa515387d5516acdd26f4c4a6ee4023e9e8eb2ad9e876d2d"
+		sha256 "37e30495da08271b6c331eb49b731f63ac557f61de7f8802ed0e42620531f31a"
 	end
 
 	# For macOS Apple Silicon (arm64)
 	if OS.mac? && Hardware::CPU.arm?
 		url "https://github.com/y3owk1n/govim/releases/download/v#{version}/govim-darwin-arm64"
-		sha256 "a02dc8157cb85129eeb6f317595149941de444f79fd4f41272605869a67d5d87"
+		sha256 "f8c43b7546035d54029ba4e2decb1be2e06442fdedc1f554105614db986cd1de"
 	end
 
 	def install
