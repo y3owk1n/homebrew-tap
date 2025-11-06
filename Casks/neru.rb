797d802f1b9e6852c5158ca5b696448caa07d43f
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-11-06 15:29:16 +0800

    fix: bin

--- Casks/neru.rb
+++ Casks/neru.rb
@@ -14,21 +14,22 @@ cask "neru" do
   homepage "https://github.com/y3owk1n/neru"
 
   app "Neru.app"
-  binary "neru"
+  binary "bin/neru"
 
   postflight do
-    # Remove quarantine attributes so macOS allows running the app and CLI
-    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/Neru.app"
-    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/neru"
+    # Remove quarantine attributes
+    system "xattr", "-d", "com.apple.quarantine", "#{appdir}/Neru.app"
+    system "xattr", "-d", "com.apple.quarantine", "#{HOMEBREW_PREFIX}/bin/neru"
 
-    # Conditional shell completions
+    # Shell completions - use the installed binary location
+    neru_binary = "#{HOMEBREW_PREFIX}/bin/neru"
     zsh_completion_dir = "#{HOMEBREW_PREFIX}/share/zsh/site-functions"
     bash_completion_dir = "#{HOMEBREW_PREFIX}/etc/bash_completion.d"
     fish_completion_dir = "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d"
 
-    system "sh", "-c", "#{staged_path}/neru completion zsh > #{zsh_completion_dir}/_neru" if Dir.exist?(zsh_completion_dir)
-    system "sh", "-c", "#{staged_path}/neru completion bash > #{bash_completion_dir}/neru" if Dir.exist?(bash_completion_dir)
-    system "sh", "-c", "#{staged_path}/neru completion fish > #{fish_completion_dir}/neru.fish" if Dir.exist?(fish_completion_dir)
+    system "sh", "-c", "#{neru_binary} completion zsh > #{zsh_completion_dir}/_neru" if Dir.exist?(zsh_completion_dir)
+    system "sh", "-c", "#{neru_binary} completion bash > #{bash_completion_dir}/neru" if Dir.exist?(bash_completion_dir)
+    system "sh", "-c", "#{neru_binary} completion fish > #{fish_completion_dir}/neru.fish" if Dir.exist?(fish_completion_dir)
   end
 
   uninstall_postflight do
