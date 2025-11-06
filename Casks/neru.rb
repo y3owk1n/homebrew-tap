ee903772e625a691caf48ebc431ac43cd0dd6b8d
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-11-06 15:24:55 +0800

    fix: add completions

--- Casks/neru.rb
+++ Casks/neru.rb
@@ -13,14 +13,28 @@ cask "neru" do
   desc "Keyboard driven navigation for macOS"
   homepage "https://github.com/y3owk1n/neru"
 
-  postflight do
-    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/Neru.app"
-    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/neru"
-  end
-
   app "Neru.app"
   binary "neru"
 
-  # Generate shell completions from Cobra CLI
-  generate_completions_from_executable(bin/"neru", "completion")
+  postflight do
+    # Remove quarantine attributes so macOS allows running the app and CLI
+    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/Neru.app"
+    system "xattr", "-d", "com.apple.quarantine", "#{staged_path}/neru"
+
+    # Conditional shell completions
+    zsh_completion_dir = "#{HOMEBREW_PREFIX}/share/zsh/site-functions"
+    bash_completion_dir = "#{HOMEBREW_PREFIX}/etc/bash_completion.d"
+    fish_completion_dir = "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d"
+
+    system "sh", "-c", "#{staged_path}/neru completion zsh > #{zsh_completion_dir}/_neru" if Dir.exist?(zsh_completion_dir)
+    system "sh", "-c", "#{staged_path}/neru completion bash > #{bash_completion_dir}/neru" if Dir.exist?(bash_completion_dir)
+    system "sh", "-c", "#{staged_path}/neru completion fish > #{fish_completion_dir}/neru.fish" if Dir.exist?(fish_completion_dir)
+  end
+
+  uninstall_postflight do
+    # Remove shell completions if they exist
+    File.delete("#{HOMEBREW_PREFIX}/share/zsh/site-functions/_neru") if File.exist?("#{HOMEBREW_PREFIX}/share/zsh/site-functions/_neru")
+    File.delete("#{HOMEBREW_PREFIX}/etc/bash_completion.d/neru") if File.exist?("#{HOMEBREW_PREFIX}/etc/bash_completion.d/neru")
+    File.delete("#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/neru.fish") if File.exist?("#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/neru.fish")
+  end
 end
