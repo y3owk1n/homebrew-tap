6d6b41a65617739aa8ee54adca761887597f3e74
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-11-06 15:06:08 +0800

    fix: neru bundle fixes
    Updated installation logic for macOS and added symlink creation for the app bundle.

--- neru.rb
+++ neru.rb
@@ -7,7 +7,7 @@ class Neru < Formula
     if Hardware::CPU.intel?
       url "https://github.com/y3owk1n/neru/releases/download/v#{version}/neru-darwin-amd64.zip"
       sha256 "c91726bab30aaa853fc727032c4f284b54b847a9043bbb4001a966e2e5dae54d"
-    elsif Hardware::CPU.arm?
+    else
       url "https://github.com/y3owk1n/neru/releases/download/v#{version}/neru-darwin-arm64.zip"
       sha256 "96d1e4139a9fac4d141cd78771af8fe0b664d89377d20815953ffe7e61b39a62"
     end
@@ -20,20 +20,29 @@ class Neru < Formula
     # Install CLI binary
     bin.install "#{base_dir}/bin/neru"
 
-    # Install the app bundle
+    # Install the app bundle inside prefix
     prefix.install "#{base_dir}/Neru.app"
 
     # Generate completions
     generate_completions_from_executable(bin/"neru", "completion")
+
+    # Create /Applications symlink
+    app_path = prefix/"Neru.app"
+    system "ln", "-sf", app_path, "/Applications/Neru.app"
   end
 
   def post_install
-    # Remove the macOS quarantine attribute from the .app bundle
+    # Remove macOS quarantine flag
     system "xattr", "-d", "com.apple.quarantine", prefix/"Neru.app" rescue nil
   end
 
-  # This tells Homebrew to link the app automatically to /Applications
-  app prefix/"Neru.app"
+  def caveats
+    <<~EOS
+      Neru.app has been installed and linked to /Applications.
+      If you prefer not to have the symlink, you can remove it with:
+        rm /Applications/Neru.app
+    EOS
+  end
 
   test do
     system "#{bin}/neru", "--version"
