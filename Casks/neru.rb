d0864c2f7b1681ed36687b01463008409c0f158f
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-03-21 11:37:21 +0800

    refactor(neru): follow homebrew official style as closely as possible

--- Casks/neru.rb
+++ Casks/neru.rb
@@ -1,17 +1,24 @@
 cask "neru" do
+  arch arm: "arm64", intel: "amd64"
+
   version "1.28.0"
 
-  if Hardware::CPU.intel?
-    url "https://github.com/y3owk1n/neru/releases/download/v#{version}/neru-darwin-amd64.zip"
-    sha256 "19dad7cdccd193362e0e66c9bf426a0c8910f9809b561fb9330f6b2e2bfc10ff"
-  else
-    url "https://github.com/y3owk1n/neru/releases/download/v#{version}/neru-darwin-arm64.zip"
-    sha256 "417c46880def8b9066b15d3df201382e3071da56cef944bb8e87f5c098dcc833"
-  end
+  sha256 arm:   "417c46880def8b9066b15d3df201382e3071da56cef944bb8e87f5c098dcc833",
+         intel: "19dad7cdccd193362e0e66c9bf426a0c8910f9809b561fb9330f6b2e2bfc10ff"
+
+  url "https://github.com/y3owk1n/neru/releases/download/v#{version}/neru-darwin-#{arch}.zip",
+      verified: "github.com/y3owk1n/neru/"
 
   name "Neru"
   desc "Keyboard driven navigation for macOS"
   homepage "https://github.com/y3owk1n/neru"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
 
   app "Neru.app"
   binary "bin/neru"
@@ -38,4 +45,6 @@ cask "neru" do
     File.delete("#{HOMEBREW_PREFIX}/etc/bash_completion.d/neru") if File.exist?("#{HOMEBREW_PREFIX}/etc/bash_completion.d/neru")
     File.delete("#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/neru.fish") if File.exist?("#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/neru.fish")
   end
+
+  zap rmdir: "~/.config/neru"
 end
