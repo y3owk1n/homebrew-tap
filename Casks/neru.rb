9f6a35c011d1d341d33a55253fab85894beec11c
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-03-21 11:51:06 +0800

    Revert "refactor(neru): remove postflight, they are all just not working.."
    This reverts commit 2f6ac5a2aefd9bf27a91a22f21cc89af7ab9d2a1.

--- Casks/neru.rb
+++ Casks/neru.rb
@@ -23,5 +23,28 @@ cask "neru" do
   app "Neru.app"
   binary "#{appdir}/Neru.app/Contents/MacOS/Neru", target: "neru"
 
+  postflight do
+    # Remove quarantine attributes
+    system "xattr", "-d", "com.apple.quarantine", "#{appdir}/Neru.app"
+    system "xattr", "-d", "com.apple.quarantine", "#{HOMEBREW_PREFIX}/bin/neru"
+
+    # Shell completions - use the installed binary location
+    neru_binary = "#{HOMEBREW_PREFIX}/bin/neru"
+    zsh_completion_dir = "#{HOMEBREW_PREFIX}/share/zsh/site-functions"
+    bash_completion_dir = "#{HOMEBREW_PREFIX}/etc/bash_completion.d"
+    fish_completion_dir = "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d"
+
+    system "sh", "-c", "#{neru_binary} completion zsh > #{zsh_completion_dir}/_neru" if Dir.exist?(zsh_completion_dir)
+    system "sh", "-c", "#{neru_binary} completion bash > #{bash_completion_dir}/neru" if Dir.exist?(bash_completion_dir)
+    system "sh", "-c", "#{neru_binary} completion fish > #{fish_completion_dir}/neru.fish" if Dir.exist?(fish_completion_dir)
+  end
+
+  uninstall_postflight do
+    # Remove shell completions if they exist
+    File.delete("#{HOMEBREW_PREFIX}/share/zsh/site-functions/_neru") if File.exist?("#{HOMEBREW_PREFIX}/share/zsh/site-functions/_neru")
+    File.delete("#{HOMEBREW_PREFIX}/etc/bash_completion.d/neru") if File.exist?("#{HOMEBREW_PREFIX}/etc/bash_completion.d/neru")
+    File.delete("#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/neru.fish") if File.exist?("#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/neru.fish")
+  end
+
   zap rmdir: "~/.config/neru"
 end
