cf5a8e18a26858445f4607b13654909e2dcffdf8
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-03-23 10:15:11 +0800

    feat(neru): update to 1.29.1

--- Casks/neru.rb
+++ Casks/neru.rb
@@ -1,10 +1,10 @@
 cask "neru" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.29.0"
+  version "1.29.1"
 
-  sha256 arm:   "ce322cd9ad7874a6f75a7537f2798ea970ca691fb186dbd991787773473aa70c",
-         intel: "1a7cbd2830edb5ce57d217b5669e9ed0ae8f84e3bf2f7874e58a6379701cd8a4"
+  sha256 arm:   "749dc0130cda7477a2a8a0791df5cc098a989ea4a7bd10305c71207ed8add73e",
+         intel: "97d401780f9cca1bcfa3c44fed5e66a6afdf48cd21540eb334539a139265ebf8"
 
   url "https://github.com/y3owk1n/neru/releases/download/v#{version}/neru-darwin-#{arch}.zip",
       verified: "github.com/y3owk1n/neru/"
