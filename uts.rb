7ba2f866330494face1311b8df80cebda14dd2bf
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2026-07-07 00:36:31 +0800

    feat(uts): update to 0.2.0

--- uts.rb
+++ uts.rb
@@ -1,18 +1,18 @@
 class Uts < Formula
 	desc "uts - single cli for compress and archive medio"
 	homepage "https://github.com/y3owk1n/uts"  # Replace with your actual URL
-  version "0.1.3"
+  version "0.2.0"
 
 	# For macOS Intel (x86_64)
 	if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/y3owk1n/uts/releases/download/v#{version}/uts-darwin-amd64.zip"
-		sha256 "62a7b1c4ef05a95096c0ace560e6ff595a3ed2b1734c5781150c74804b59c238"
+		sha256 "b1ebe7585a92a23440c06978518d25187feeeb766983bd7d6f14e502fc6ef2b5"
 	end
 
 	# For macOS Apple Silicon (arm64)
 	if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/y3owk1n/uts/releases/download/v#{version}/uts-darwin-arm64.zip"
-		sha256 "3b299658aca93938947c6d477ef2351739405783fb2732fe181479637984e0e5"
+		sha256 "f515dcd52f36c4b0979f1839a5e165d3138c03122321188b7a1bce7c7c261b0b"
 	end
 
 	def install
